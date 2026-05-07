9cd7293354eee3186b342957b74d98226db10230
Anna Yee <annah@b98-aruba-authenticated-10-110-164-180.wlan.cpp.edu>
2026-05-07 13:31:48 -0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Nanner was here'));
+app.get('/', (req, res) => res.send('Nanner is almost done with this assignment!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
